00b579b89a1ed3ed7c96d7da3f33e4679c34b579
Francis <fraanciisq@gmail.com>
2024-02-15 17:31:05 +0800

    cart-count

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -19,6 +19,7 @@ const Navbar = () => {
         <div className='nav-login-cart'>
             <button>Login</button>
             <img src={cart_icon} alt='' />
+            <div className="nav-cart-count">0</div>
         </div>
     </div>
   )
