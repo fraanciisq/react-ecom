3a1d84030f13d1682b3c9ec5067cc085de1e9c70
Francis <fraanciisq@gmail.com>
2024-02-15 17:55:30 +0800

    api-font

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
+
+  const [menu,setMenu] = useState("shop");
+     
   return (
     <div className='navbar'>
         <div className="nav-logo">
@@ -11,10 +14,10 @@ const Navbar = () => {
             <p>Riri Shop</p>
         </div>
         <ul className='nav-menu'>
-            <li>Shop <hr/></li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>} {menu==="shop?"?<h/>:<></>}</li>
+            <li onClick={()=>{setMenu("men")}}>Men{menu==="men"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("women")}}>Women{menu==="women"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
         </ul>
         <div className='nav-login-cart'>
             <button>Login</button>
