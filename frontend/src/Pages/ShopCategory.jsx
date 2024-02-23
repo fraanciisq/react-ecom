1f187c3e2fbc3ca0682ea8f90fd6d31f24a42deb
Francis <fraanciisq@gmail.com>
2024-02-23 22:11:09 +0800

    context-banner

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const {all_product} = useContext(ShopContext);
   return (
-    <div>ShopCategory</div>
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
+    </div>
   )
 }
 
