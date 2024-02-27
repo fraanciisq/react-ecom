8308a93296fc64a8b338a20f3ab53ae721bc2ccc
Francis <fraanciisq@gmail.com>
2024-02-27 18:06:39 +0800

    fixed-kds

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
         <Route path='/' element = {<Shop/>}/>
         <Route path='/men' element = {<ShopCategory banner={men_banner} category="men"/>}/>
         <Route path='/women' element = {<ShopCategory banner={women_banner}category="women"/>}/>
-        <Route path='/kids' element = {<ShopCategory banner={kid_banner} category="kids"/>}/>
+        <Route path='/kids' element = {<ShopCategory banner={kid_banner} category="kid"/>}/>
         <Route path='/product' element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>
