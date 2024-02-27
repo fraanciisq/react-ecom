b2006352664af840c10cce842b8eecdaee01b493
Francis <fraanciisq@gmail.com>
2024-02-27 18:09:50 +0800

    footer-pnk

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css'
-import footer_logo from '../Assets/logo_big.png'
+import footer_logo from '../Assets/riri_logo2.png'
 import instagram_icon from '../Assets/instagram_icon.png'
 import pintester_icon from  '../Assets/pintester_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
@@ -11,7 +11,7 @@ const Footer = () => {
     <div className='footer'>
         <div className="footer-logo">
             <img src={footer_logo} alt="" />
-            <p>SHOPPER</p>
+            <p>Check Out Now at Riri Shop</p>
             
         </div>
         <ul className="footer-links">
