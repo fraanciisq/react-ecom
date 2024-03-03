93f2b8e824d3bca43da4cfbd4d5f284e20382ede
Francis <fraanciisq@gmail.com>
2024-03-04 00:11:17 +0800

    product-display

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrumb from '../Components/Breadcrumbs/Breadcrumb'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
@@ -10,6 +11,7 @@ const Product = () => {
   return (
     <div>
         <Breadcrumb product={product}/>
+        <ProductDisplay product={product} />
     </div>
   )
 }
