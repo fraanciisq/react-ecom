759631ad15df36b628de23f0820114a4ba8f7ba1
Francis <fraanciisq@gmail.com>
2024-03-03 22:36:56 +0800

    fixed-footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'; // Import useLocation
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
@@ -11,24 +11,28 @@ import men_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
 import kid_banner from './Components/Assets/banner_kids.png'
 
+function FooterWithLocation() {
+  const location = useLocation();
+  return location.pathname !== '/login' && <Footer />;
+}
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element = {<Shop/>}/>
-        <Route path='/men' element = {<ShopCategory banner={men_banner} category="men"/>}/>
-        <Route path='/women' element = {<ShopCategory banner={women_banner}category="women"/>}/>
-        <Route path='/kids' element = {<ShopCategory banner={kid_banner} category="kid"/>}/>
-        <Route path='/product' element={<Product/>}>
-          <Route path=':productId' element={<Product/>}/>
-        </Route>
-        <Route path='/cart' element ={<Cart/>}/>
-        <Route path='/login' element = {<LoginSignup/>}/>
-      </Routes>
-      <Footer/>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/men' element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path='/women' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+        <FooterWithLocation />
       </BrowserRouter>
     </div>
   );
