9f41d3fc8202dd473a2719c2b2a893944126f6ae
Francis <fraanciisq@gmail.com>
2024-03-27 17:07:59 +0800

    tofx-shoptotalcontext

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../../Context/ShopContext'
 import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
-    const {all_product,cartItems,removeFromCart} = useContext(ShopContext);
+    const {getTotalCartAmount,all_product,cartItems,removeFromCart} = useContext(ShopContext);
   return (
     <div className='cartitems'>
         <div className="cartitems-format-main">
@@ -41,7 +41,7 @@ const CartItems = () => {
                 <div>
                     <div className="cartitems-total-item">
                         <p>Subtotal</p>
-                        <p>₱{0}</p>
+                        <p>₱{getTotalCartAmount}</p>
                     </div>
                     <hr />
                     <div className="cartitems-total-item">
@@ -51,7 +51,7 @@ const CartItems = () => {
                     <hr />
                     <div className="cartitems-total-item">
                         <h3>Total</h3>
-                        <h3>₱{0}</h3>
+                        <h3>₱{getTotalCartAmount}</h3>
                     </div>
                 </div>
                 <button>PROCEED TO CHECKOUT</button>
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -26,7 +26,21 @@ const ShopContextProvider = (props) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
 
-    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in cartItems)
+        {
+            if(cartItems[item]>0)
+            {
+                let itemInfo = all_product.find((product)=>product.id===Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }
+            return totalAmount;
+        }
+       
+    }
+
+    const contextValue = {getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
